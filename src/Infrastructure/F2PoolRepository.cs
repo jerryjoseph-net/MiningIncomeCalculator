3dd84a954df53016b7d952a2d3ca8244b183c16c
Jerry Joseph <jerryjoseph.net@gmail.com>
2023-01-04 02:38:14 +0100

    Read CSV file

---
+++ src/Infrastructure/F2PoolRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace MiningIncomeCalculator.Infrastructure;
+
+public class F2PoolRepository
+{
+    public List<Payout> GetPayoutData(string csvFilePath)
+    {
+        var payouts = new List<Payout>();
+
+        using var reader = new StreamReader(csvFilePath);
+        
+        while (!reader.EndOfStream)
+        {
+            var line = reader.ReadLine();
+            var values = line?.Split(',');
+
+            if (values != null && values[0] != "Date") // Skip header row
+            {
+                payouts.Add(new Payout(values[0], values[1], values[2], values[3], values[4]));
+            }
+        }
+
+        return payouts;
+    }
+}
+
+public record Payout(string Date, string Amount, string Address, string Status, string TxId);
---
+++ tests/Infrastructure.IntegrationTests/F2PoolRepositoryTests.cs
@@ -0,0 +1,18 @@
+﻿namespace MiningIncomeCalculator.Infrastructure.IntegrationTests;
+
+public class F2PoolRepositoryTests
+{
+    [Fact]
+    public void GetPayoutData_FakeFile_ReturnsPayouts()
+    {
+        // Arrange 
+        var sut = new F2PoolRepository();
+
+        // Act 
+        var result = sut.GetPayoutData(Directory.GetCurrentDirectory() + "\\Payout-BTC-Fake.csv");
+
+        // Assert 
+        Assert.NotNull(result);
+        Assert.Equal(5, result.Count);
+    }
+}
